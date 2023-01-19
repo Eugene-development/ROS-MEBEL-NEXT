c2cd35b57a6781d619a79a3c56808cf88a3b41e9
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-19 18:08:40 +0300

    c

--- app/src/pages/index.jsx
+++ app/src/pages/index.jsx
@@ -3,6 +3,7 @@ import MainLayout from '@/components/Layouts/MainLayout';
 import Hero from '@/components/Main/Hero'
 import { PrimaryFeatures } from '@/components/Main/PrimaryFeatures';
 import Brand from '@/components/Main/Brand';
+import { Testimonials } from '@/components/Main/Testimonials';
 
 export default function Home() {
     return (
@@ -10,11 +11,13 @@ export default function Home() {
             <MainLayout>
                 <Head>
                     <title>Мебель на заказ</title>
+                    <meta name="description" content="This is a sample tag description for SEO." />
                 </Head>
 
                 <Hero/>
                 <Brand/>
                 <PrimaryFeatures/>
+                <Testimonials/>
             </MainLayout>
         </>
     )
