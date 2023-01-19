ee8f7b558b77d4eb46943ae35055d6522cce9d18
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-19 22:30:25 +0300

    c

--- app/src/components/Main/Forms/Measurement/index.jsx
+++ app/src/components/Main/Forms/Measurement/index.jsx
@@ -20,48 +20,18 @@ import { useReactiveVar } from '@apollo/client'
 import { is_visible_form_measurement } from '@/apollo/stores/visible'
 
 
-const team = [
-  {
-    name: 'Tom Cook',
-    email: 'tom.cook@example.com',
-    href: '#',
-    imageUrl:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Whitney Francis',
-    email: 'whitney.francis@example.com',
-    href: '#',
-    imageUrl:
-      'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Leonard Krasner',
-    email: 'leonard.krasner@example.com',
-    href: '#',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Floyd Miles',
-    email: 'floyd.miles@example.com',
-    href: '#',
-    imageUrl:
-      'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Emily Selman',
-    email: 'emily.selman@example.com',
-    href: '#',
-    imageUrl:
-      'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-]
-
 export default function FormMeasurement() {
   const isVisibleFormMeasurement = useReactiveVar(is_visible_form_measurement)
+  const [name, setName] = useState();
 
-  const [open, setOpen] = useState(isVisibleFormMeasurement)
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = { name };
+    // send form data to server
+    console.log(formData);
+    }
+
+//   const [open, setOpen] = useState(isVisibleFormMeasurement)
 
   return (
     <Transition.Root show={isVisibleFormMeasurement} as={Fragment}>
@@ -81,7 +51,7 @@ export default function FormMeasurement() {
                 leaveTo="translate-x-full"
               >
                 <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                  <form className="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl">
+                  <form onSubmit={handleSubmit} className="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl">
                     <div className="h-0 flex-1 overflow-y-auto">
                       <div className="bg-green-700 py-6 px-4 sm:px-6">
                         <div className="flex items-center justify-between">
@@ -114,6 +84,7 @@ export default function FormMeasurement() {
                               <div className="mt-1">
                                 <input
                                   required
+                                  value={name} onChange={e => setName(e.target.value)}
                                   type="text"
                                   name="nameForm"
                                   id="name"
