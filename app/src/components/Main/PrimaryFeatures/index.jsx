a74c08bce4701fd201724bc48ebbf0330742fa21
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-19 22:52:25 +0300

    c

--- app/src/components/Main/PrimaryFeatures/index.jsx
+++ app/src/components/Main/PrimaryFeatures/index.jsx
@@ -134,7 +134,6 @@ export default function PrimaryFeatures() {
                         className="w-full"
                         src={feature.image}
                         alt=""
-                        priority
                         sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
                       />
                     </div>
