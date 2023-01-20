6a41ffeb5ae444bba47710ce49919f32f36d386e
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-20 19:56:01 +0300

    с

---
+++ app/src/pages/actions/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import MainLayout from '@/components/Layouts/MainLayout';
+import Action from "@/components/Main/Action"
+
+export default function Actions () {
+    return (
+        <>
+            <MainLayout>
+                <Head>
+                    <title>{process.env.NEXT_PUBLIC_COMPANY} | Акции</title>
+                </Head>
+
+                <Action/>
+            </MainLayout>
+        </>
+    )
+}
