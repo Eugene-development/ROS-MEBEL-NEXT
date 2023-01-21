22cc4f354bd54e8cd2a0806fbcb63e5017072ffc
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-21 18:41:20 +0300

    c

--- app/src/pages/kuhni/index.jsx
+++ app/src/pages/kuhni/index.jsx
@@ -7,37 +7,43 @@ const types = [
 		name: 'МДФ',
 		description: 'Кухонные гарнитуры с фасадами из МДФ с покрытием ПВХ плёнкой',
 		href: 'kuhni/mdf',
-		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/elena.jpeg'
+		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/elena.jpeg',
+        imageAlt: 'Шкафы',
 	},
 	{
 		name: 'Патина',
 		description: 'Кухонные гарнитуры с фасадами из МДФ с патинированием фасадов',
 		href: 'kuhni/patina',
-		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/elina.jpeg'
+		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/elina.jpeg',
+        imageAlt: 'Шкафы',
 	},
 	{
 		name: 'Пластик',
 		description: 'Кухонные гарнитуры с фасадами из пластика в профиле или 3d-кромке',
 		href: 'kuhni/plastic',
-		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/primula.jpeg'
+		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/primula.jpeg',
+        imageAlt: 'Шкафы',
 	},
 	{
 		name: 'Эмаль',
 		description: 'Кухонные гарнитуры с эмалированными фасадами разнообразной палитры',
 		href: 'kuhni/emal',
-		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/kapuchino.jpeg'
+		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/kapuchino.jpeg',
+        imageAlt: 'Шкафы',
 	},
 	{
 		name: 'Акрил',
 		description: 'Варианты кухонных гарнитуров с акриловыми фасадами с высоким глянцем',
 		href: 'kuhni/akril',
-		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/scarlet.jpeg'
+		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/scarlet.jpeg',
+        imageAlt: 'Шкафы',
 	},
 	{
 		name: 'ЛДСП',
 		description: 'Модули кухонных гарнитуров эконом класса стандартных размеров и под заказ',
 		href: 'kuhni/ldsp',
-		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/milena.jpeg'
+		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/milena.jpeg',
+        imageAlt: 'Шкафы',
 	}
 ];
 
--- app/src/pages/shkafi/index.jsx
+++ app/src/pages/shkafi/index.jsx
@@ -7,37 +7,44 @@ const types = [
 		name: 'Шкафы купе',
 		description: 'Шкафы купе с подвесной или нижнеопорной системой высокого качества',
 		href: 'shkafy/kupe',
-		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/1.jpg'
+		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/1.jpg',
+        imageAlt: 'Шкафы',
 	},
 	{
 		name: 'Распашные шкафы',
 		description: 'Пеналы и двух, трёх и четырёх створчатые шкафы с выдвижными ящиками',
 		href: 'shkafy/raspashnie',
-		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/shkafi/halcedon.jpeg'
+		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/shkafi/halcedon.jpeg',
+        imageAlt: 'Шкафы',
+
 	},
 	{
 		name: 'Встроенные шкафы',
 		description: 'Безкаркасные шкафы встроенные в нишу с полками, ящиками и специальной фурнитурой',
 		href: 'shkafy/vstroenie',
-		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/5.jpg'
+		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/5.jpg',
+        imageAlt: 'Шкафы',
 	},
 	{
 		name: 'Гардеробные',
 		description: 'Полнофункциональные гардеробные с любым наполнением и под любой размер',
 		href: 'shkafy/garderobnie',
-		imageSrc: 'https://мебель-по-цене-фабрики.рф/thumb/2/yoya_PxK9Ckv3eGUUTy1Pg/r/d/mebel_dlya_garderobnoy_komnaty_syuzen.jpg'
+		imageSrc: 'https://мебель-по-цене-фабрики.рф/thumb/2/yoya_PxK9Ckv3eGUUTy1Pg/r/d/mebel_dlya_garderobnoy_komnaty_syuzen.jpg',
+        imageAlt: 'Шкафы',
 	},
 	{
 		name: 'Двери купе',
 		description: 'Двери купе в качестве перегородок или для закрытия ниш и гардеробных комнат',
 		href: 'shkafy/dveri',
-		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/shkafi/zhasmin.jpeg'
+		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/shkafi/zhasmin.jpeg',
+        imageAlt: 'Шкафы',
 	},
 	{
 		name: 'Офисные шкафы',
 		description: 'Стильные и строгие варианты шкафов для офисных помещений или для вашего дома',
 		href: 'shkafy/ofisnie',
-		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/7.jpg'
+		imageSrc: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/7.jpg',
+        imageAlt: 'Шкафы',
 	}
 ];
 
