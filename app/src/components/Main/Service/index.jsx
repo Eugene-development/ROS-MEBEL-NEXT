84c1d1c428cc9f21b3316c7ffa8529a944734bc3
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-21 20:33:41 +0300

    с

--- app/src/components/Main/Service/index.jsx
+++ app/src/components/Main/Service/index.jsx
@@ -7,11 +7,11 @@ export default function Service () {
         <section aria-labelledby="details-heading">
           <div className="flex flex-col items-center text-center">
             <h2 id="details-heading" className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              The Fine Details
+              Мебель на заказ
             </h2>
             <p className="mt-3 max-w-3xl text-lg text-gray-600">
-              Our patented padded snack sleeve construction protects your favorite treats from getting smooshed during
-              all-day adventures, long shifts at work, and tough travel schedules.
+            Мы сотрудничаем только с проверенными производителями корпусной мебели и фурнитуры, дающие гарантии качества и адекватные цены на продукцию. <br/> Предоставляем услуги по замеру и установке мебели.
+
             </p>
           </div>
 
@@ -19,27 +19,37 @@ export default function Service () {
             <div>
               <div className="aspect-w-3 aspect-h-2 w-full overflow-hidden rounded-lg">
                 <img
-                  src="https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg"
-                  alt="Drawstring top with elastic loop closure and textured interior padding."
+                  src="https://storage.yandexcloud.net/brand-logo/azbuka-komforta/benefits/benefit-1.jpeg"
+                  alt="Замерщик мерит расстояние"
                   className="h-full w-full object-cover object-center"
                 />
               </div>
               <p className="mt-8 text-base text-gray-500">
-                The 20L model has enough space for 370 candy bars, 6 cylinders of chips, 1220 standard gumballs, or any
-                combination of on-the-go treats that your heart desires. Yes, we did the math.
+                Выезжаем в удобное для вас время, предварительно согласовав дату. При вашем желании можем выехать на обмер за город.
+              </p>
+              <p className="mt-8 text-base text-gray-500">
+                Работаем высокоточным лазерным измерительным инструментом. Учитываем неровности поверхностей и разность горизонталей.
+              </p>
+              <p className="mt-8 text-base text-gray-500">
+                Фиксируем данные в дизайн-проекте и несём ответственность за соответствие размеров в проекте и по фактическому исполнению.
               </p>
             </div>
             <div>
               <div className="aspect-w-3 aspect-h-2 w-full overflow-hidden rounded-lg">
                 <img
-                  src="https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-02.jpg"
-                  alt="Front zipper pouch with included key ring."
+                  src="https://storage.yandexcloud.net/brand-logo/azbuka-komforta/benefits/benefit-2.jpeg"
+                  alt="Сборщик собирает гарнитур"
                   className="h-full w-full object-cover object-center"
                 />
               </div>
               <p className="mt-8 text-base text-gray-500">
-                Up your snack organization game with multiple compartment options. The quick-access stash pouch is ready
-                for even the most unexpected snack attacks and sharing needs.
+                Выполняем сборку и установку мебели в кратчайшие сроки, предварительно обговорив удобную дату начала и окончания работ.
+              </p>
+              <p className="mt-8 text-base text-gray-500">
+                Используем качественный ручной инструмент. Поддерживаем чистоту на объекте и соблюдаем тишину в установленное время.
+              </p>
+              <p className="mt-8 text-base text-gray-500">
+                Профессионально монтируем сетчатые корзины и ящики на роликовых или шариковых направляющих с доводчиком и без него.
               </p>
             </div>
           </div>
