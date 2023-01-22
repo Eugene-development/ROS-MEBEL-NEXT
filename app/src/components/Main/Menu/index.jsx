744081c24ae7fce7caba20b2578fcf7c218239e9
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-22 19:38:12 +0300

    c

--- app/src/components/Main/Menu/index.jsx
+++ app/src/components/Main/Menu/index.jsx
@@ -44,8 +44,8 @@ const Menu = () => {
                             className="flex rounded-lg px-3 py-1.5 text-xl font-semibold leading-6 text-gray-50 shadow-sm ring-2 ring-gray-50/10 hover:ring-gray-100/20"
                         >
                             <PhoneIcon className="block h-6 w-6" aria-hidden="true" />
-                            <Link href="tel:79306711455" className="ml-2 text-gray-50 text-xs sm:text-lg font-bold "
-                                >8 (930) 671-14-55
+                            <Link href="tel:79801506943" className="ml-2 text-gray-50 text-xs sm:text-lg font-bold "
+                                >8 (980) 150-69-43
                             </Link>
                         </div>
                     </div>
