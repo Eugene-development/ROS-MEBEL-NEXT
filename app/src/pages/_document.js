fba6c1864dc4036f5225e6e90dfc026f2c1b3cb1
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-22 20:55:39 +0300

    c

--- app/src/pages/_document.js
+++ app/src/pages/_document.js
@@ -14,7 +14,44 @@ class MyDocument extends Document {
                         href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap"
                         rel="stylesheet"
                     />
+                    <link
+                        rel="icon"
+                        href="https://storage.yandexcloud.net/brand-logo/mos-mebel/favicon.svg"
+                    />
+
+                    <script
+                        type="text/javascript"
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                    (function (m, e, t, r, i, k, a) {
+                                        m[i] =
+                                            m[i] ||
+                                            function () {
+                                                (m[i].a = m[i].a || []).push(arguments);
+                                            };
+                                        m[i].l = 1 * new Date();
+                                        for (var j = 0; j < document.scripts.length; j++) {
+                                            if (document.scripts[j].src === r) {
+                                                return;
+                                            }
+                                        }
+                                        (k = e.createElement(t)),
+                                            (a = e.getElementsByTagName(t)[0]),
+                                            (k.async = 1),
+                                            (k.src = r),
+                                            a.parentNode.insertBefore(k, a);
+                                    })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+                                    ym(51188858, 'init', {
+                                        clickmap: true,
+                                        trackLinks: true,
+                                        accurateTrackBounce: true
+                                    });
+                                `,
+                        }}
+                    />
                 </Head>
+
                 <body className="antialiased">
                     <Main />
                     <NextScript />
