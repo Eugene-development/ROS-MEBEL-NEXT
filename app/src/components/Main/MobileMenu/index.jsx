3ded51908b4ccb1100ee342e8e98e8a5d451edc9
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-23 11:40:28 +0300

    c

--- app/src/components/Main/MobileMenu/index.jsx
+++ app/src/components/Main/MobileMenu/index.jsx
@@ -51,25 +51,25 @@ const callsToAction = [
 const resources = [
   {
     name: 'Замер',
-    description: 'Get all of your questions answered in our forums or contact support.',
+    description: 'Ответы по вопросам с замерами помещений',
     href: '/information/zamer',
     icon: LifebuoyIcon,
   },
   {
     name: 'Проект',
-    description: 'Learn how to maximize our platform to get the most out of it.',
+    description: 'Информация по проектированию мебели',
     href: '/information/proekt',
     icon: BookmarkSquareIcon,
   },
   {
     name: 'Доставка',
-    description: 'See what meet-ups and other events we might be planning near you.',
+    description: 'Условия доставки ваших заказов',
     href: '/information/dostavka',
     icon: CalendarIcon,
   },
-  { name: 'Сборка', description: 'Understand how we take your privacy seriously.', href: '/information/ustanovka', icon: ShieldCheckIcon },
-  { name: 'Оплата', description: 'Understand how we take your privacy seriously.', href: '/information/oplata', icon: ShieldCheckIcon },
-  { name: 'Гарантии', description: 'Understand how we take your privacy seriously.', href: '/information/garantii', icon: ShieldCheckIcon },
+  { name: 'Сборка', description: 'Общая информация по сборке и установке', href: '/information/ustanovka', icon: ShieldCheckIcon },
+  { name: 'Оплата', description: 'Правила оплаты заказанной мебели', href: '/information/oplata', icon: ShieldCheckIcon },
+  { name: 'Гарантии', description: 'Предоставляемые гарантии на всю продукцию', href: '/information/garantii', icon: ShieldCheckIcon },
 ]
 const recentPosts = [
   { id: 1, name: 'Boost your conversion rate', href: '#' },
@@ -83,48 +83,48 @@ function classNames(...classes) {
 
 export default function MobileMenu() {
   return (
-    <div className="relative bg-gray-50">
-      <Popover className="relative bg-white shadow">
-        <div className="mx-auto max-w-7xl px-6">
-          <div className="flex items-center justify-between py-6 md:justify-start md:space-x-10">
+    <div className="relative bg-gray-900">
+      <Popover className="relative bg-gray-900 shadow">
+        <div className="mx-auto max-w-full px-6">
+          <div className="flex items-center justify-between py-3 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
-              <a href="#">
+              <Link href="/">
                 <span className="sr-only">Your Company</span>
                 <img
                   className="h-8 w-auto sm:h-10"
                   src="https://storage.yandexcloud.net/brand-logo/azbuka-komforta/logo/logo2.png"
                   alt="logo"
                 />
-              </a>
+              </Link>
             </div>
             <div className="-my-2 -mr-2 md:hidden">
-              <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
+              <Popover.Button className="inline-flex items-center justify-center rounded-md bg-gray-900 p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
                 <span className="sr-only">Open menu</span>
                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
               </Popover.Button>
             </div>
             <Popover.Group as="nav" className="hidden space-x-10 md:flex">
 
-              <a href="" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <Link href="" className="text-base font-medium text-gray-50 hover:text-gray-100">
                 Главная
-              </a>
-              <a href="/catalog" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              </Link>
+              <Link href="/catalog" className="text-base font-medium text-gray-50 hover:text-gray-100">
                 Каталог
-              </a>
-              <a href="/actions" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              </Link>
+              <Link href="/actions" className="text-base font-medium text-gray-50 hover:text-gray-100">
                 Акции
-              </a>
-              <a href="/blog" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              </Link>
+              <Link href="/blog" className="text-base font-medium text-gray-50 hover:text-gray-100">
                 Блог
-              </a>
+              </Link>
 
               <Popover className="relative">
                 {({ open }) => (
                   <>
                     <Popover.Button
                       className={classNames(
-                        open ? 'text-gray-900' : 'text-gray-500',
-                        'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+                        open ? 'text-gray-50' : 'text-gray-50',
+                        'px-2 group inline-flex items-center rounded-md bg-gray-900 text-base font-medium hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
                       )}
                     >
                       <span>Информация</span>
@@ -148,39 +148,39 @@ export default function MobileMenu() {
                     >
                       <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-md -translate-x-1/2 transform px-2 sm:px-0">
                         <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                          <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                          <div className="relative grid gap-5 bg-white px-5 py-6 sm:gap-5 sm:p-8">
                             {resources.map((item) => (
-                              <a
+                              <Link
                                 key={item.name}
                                 href={item.href}
                                 className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
                               >
-                                <item.icon className="h-6 w-6 flex-shrink-0 text-blue-600" aria-hidden="true" />
+                                <item.icon className="h-6 w-6 flex-shrink-0 text-red-900" aria-hidden="true" />
                                 <div className="ml-4">
-                                  <p className="text-base font-medium text-gray-900">{item.name}67</p>
+                                  <p className="text-base font-medium text-gray-900">{item.name}</p>
                                   <p className="mt-1 text-sm text-gray-500">{item.description}</p>
                                 </div>
-                              </a>
+                              </Link>
                             ))}
                           </div>
                           <div className="bg-gray-50 px-5 py-5 sm:px-8 sm:py-8">
                             <div>
-                              <h3 className="text-base font-medium text-gray-500">Recent Posts</h3>
+                              <h3 className="text-base font-medium text-gray-500">Последние посты</h3>
                               <ul role="list" className="mt-4 space-y-4">
                                 {recentPosts.map((item) => (
                                   <li key={item.id} className="truncate text-base">
-                                    <a href={item.href} className="font-medium text-gray-900 hover:text-gray-700">
+                                    <Link href={item.href} className="font-medium text-gray-900 hover:text-gray-700">
                                       {item.name}
-                                    </a>
+                                    </Link>
                                   </li>
                                 ))}
                               </ul>
                             </div>
                             <div className="mt-5 text-sm">
-                              <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
-                                View all posts
+                              <Link href="#" className="font-medium text-red-900 hover:text-red-900">
+                                Посмотреть все посты
                                 <span aria-hidden="true"> &rarr;</span>
-                              </a>
+                              </Link>
                             </div>
                           </div>
                         </div>
@@ -190,17 +190,16 @@ export default function MobileMenu() {
                 )}
               </Popover>
             </Popover.Group>
-            <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-              <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-                Sign in
-              </a>
-              <a
-                href="#"
-                className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700"
-              >
-                Sign up
-              </a>
-            </div>
+                    <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
+                        <div
+                            className="flex rounded-lg px-3 py-1.5 text-xl font-semibold leading-6 text-gray-50 shadow-sm ring-2 ring-gray-50/10 hover:ring-gray-100/20"
+                        >
+                            <PhoneIcon className="block h-6 w-6" aria-hidden="true" />
+                            <Link href="tel:79801506943" className="ml-2 text-gray-50 text-xs sm:text-lg font-bold "
+                                >8 (980) 150-69-43
+                            </Link>
+                        </div>
+                    </div>
           </div>
         </div>
 
