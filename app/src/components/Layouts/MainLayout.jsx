76b69b41fc322d3e564e0961ea4cd76229a0c619
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-23 11:18:40 +0300

    с

--- app/src/components/Layouts/MainLayout.jsx
+++ app/src/components/Layouts/MainLayout.jsx
@@ -2,11 +2,14 @@ import Header from './Header';
 import Footer from './Footer';
 import Banner from '../Main/Banner';
 import FormMeasurement from '@/components/Main/Forms/Measurement';
+import MobileMenu from '../Main/MobileMenu';
 
 const MainLayout = ({ children }) => {
 
     return (
         <div className="min-h-screen">
+            <MobileMenu/>
+
             <Banner/>
             <Header/>
                 <main>{children}</main>
