5f9386c892865dcc5fe695f7e135e916d3039be4
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-23 12:33:56 +0300

    c

--- app/src/components/Main/Hero/index.jsx
+++ app/src/components/Main/Hero/index.jsx
@@ -50,7 +50,7 @@ export default function Hero() {
       </div> */}
       <main>
         <div className="relative px-6 lg:px-8">
-          <div className="mx-auto max-w-7xl pt-10 pb-16 sm:pt-16 sm:pb-16">
+          <div className="mx-auto max-w-7xl pt-12 pb-16 sm:pt-20 sm:pb-16">
             <div>
               <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                 <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
--- app/src/components/Main/MobileMenu/index.jsx
+++ app/src/components/Main/MobileMenu/index.jsx
@@ -87,7 +87,7 @@ export default function MobileMenu() {
     <div className="relative bg-gray-900">
       <Popover className="relative bg-gray-900 shadow">
         <div className="mx-auto max-w-full px-6">
-          <div className="flex items-center justify-between py-3 md:justify-start md:space-x-10">
+          <div className="flex items-center justify-between py-3 lg:justify-start lg:space-x-8">
             <div className="flex justify-start lg:w-0 lg:flex-1">
               <Link href="/">
                 <span className="sr-only">Your Company</span>
@@ -98,22 +98,19 @@ export default function MobileMenu() {
                 />
               </Link>
             </div>
-            <div className="-my-2 -mr-2 md:hidden">
+            <div className="-my-2 -mr-2 lg:hidden">
               <Popover.Button className="inline-flex items-center justify-center rounded-md bg-gray-900 p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
                 <span className="sr-only">Open menu</span>
                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
               </Popover.Button>
             </div>
-            <Popover.Group as="nav" className="hidden space-x-10 md:flex">
+            <Popover.Group as="nav" className="hidden space-x-9 lg:flex">
 
-              <Link href="" className="text-base font-medium text-gray-50 hover:text-gray-100">
+              <Link href="/" className="text-base font-medium text-gray-50 hover:text-gray-100">
                 Главная
               </Link>
               <Link href="/catalog" className="text-base font-medium text-gray-50 hover:text-gray-100">
                 Каталог
-              </Link>
-              <Link href="/actions" className="text-base font-medium text-gray-50 hover:text-gray-100">
-                Акции
               </Link>
               <Link href="/blog" className="text-base font-medium text-gray-50 hover:text-gray-100">
                 Блог
@@ -150,16 +147,16 @@ export default function MobileMenu() {
                       leaveFrom="opacity-100 translate-y-0"
                       leaveTo="opacity-0 translate-y-1"
                     >
-                      <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-md -translate-x-1/2 transform px-2 sm:px-0">
+                      <Popover.Panel className="absolute left-1/2 z-30 mt-3 w-screen max-w-md -translate-x-1/2 transform px-2 sm:px-0 ">
                         <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                          <div className="relative grid gap-5 bg-white px-5 py-6 sm:gap-5 sm:p-8">
+                          <div className="relative grid gap-5 bg-gray-100 px-5 py-6 sm:gap-5 sm:p-8">
                             {resources.map((item) => (
                               <Link
                                 key={item.name}
                                 href={item.href}
                                 className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
                               >
-                                <item.icon className="h-6 w-6 flex-shrink-0 text-red-900" aria-hidden="true" />
+                                {/* <item.icon className="h-6 w-6 flex-shrink-0 text-red-900" aria-hidden="true" /> */}
                                 <div className="ml-4">
                                   <p className="text-base font-medium text-gray-900">{item.name}</p>
                                   <p className="mt-1 text-sm text-gray-500">{item.description}</p>
@@ -167,7 +164,7 @@ export default function MobileMenu() {
                               </Link>
                             ))}
                           </div>
-                          <div className="bg-gray-50 px-5 py-5 sm:px-8 sm:py-8">
+                          {/* <div className="bg-gray-50 px-5 py-5 sm:px-8 sm:py-8">
                             <div>
                               <h3 className="text-base font-medium text-gray-500">Последние посты</h3>
                               <ul role="list" className="mt-4 space-y-4">
@@ -186,7 +183,7 @@ export default function MobileMenu() {
                                 <span aria-hidden="true"> &rarr;</span>
                               </Link>
                             </div>
-                          </div>
+                          </div> */}
                         </div>
                       </Popover.Panel>
                     </Transition>
@@ -199,7 +196,7 @@ export default function MobileMenu() {
                             className="flex rounded-lg px-3 py-1.5 text-xl font-semibold leading-6 text-gray-50 shadow-sm ring-2 ring-gray-50/10 hover:ring-gray-100/20"
                         >
                             <PhoneIcon className="block h-6 w-6" aria-hidden="true" />
-                            <Link href="tel:79801506943" className="ml-2 text-gray-50 text-xs sm:text-lg font-bold "
+                            <Link href="tel:79801506943" className="ml-2 text-gray-50 text-xs lg:text-lg font-bold "
                                 >8 (980) 150-69-43
                             </Link>
                         </div>
@@ -218,7 +215,7 @@ export default function MobileMenu() {
         >
           <Popover.Panel
             focus
-            className="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden"
+            className="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition lg:hidden"
           >
             <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
               <div className="px-5 pt-5 pb-6">
@@ -240,14 +237,14 @@ export default function MobileMenu() {
                 <div className="mt-6">
                   <nav className="grid gap-y-8">
                     {resources.map((item) => (
-                      <a
+                      <Link
                         key={item.name}
                         href={item.href}
                         className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
                       >
                         <item.icon className="h-6 w-6 flex-shrink-0 text-blue-500" aria-hidden="true" />
                         <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
-                      </a>
+                      </Link>
                     ))}
                   </nav>
                 </div>
@@ -255,13 +252,13 @@ export default function MobileMenu() {
               <div className="space-y-6 py-6 px-5">
                 <div className="grid grid-cols-2 gap-y-4 gap-x-8">
                   {resources.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
                       href={item.href}
                       className="text-base font-medium text-gray-900 hover:text-gray-700"
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
                     <div className="lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
