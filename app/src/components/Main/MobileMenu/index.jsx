d733b396212e465188da21f4053361c40ee6afa6
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-23 12:59:17 +0300

    c

--- app/src/components/Main/MobileMenu/index.jsx
+++ app/src/components/Main/MobileMenu/index.jsx
@@ -233,7 +233,7 @@ export default function MobileMenu() {
                         href={item.href}
                         className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
                       >
-                        <item.icon className="h-6 w-6 flex-shrink-0 text-blue-500" aria-hidden="true" />
+                        <item.icon className="h-6 w-6 flex-shrink-0 text-gray-800" aria-hidden="true" />
                         <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
                       </Link>
                     ))}
