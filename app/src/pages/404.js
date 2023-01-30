e60404428640b0689fca00e005a3e7c9225e06c3
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-30 12:19:59 +0300

    с

--- app/src/pages/404.js
+++ app/src/pages/404.js
@@ -5,25 +5,34 @@ const NotFoundPage = () => {
     const router = useRouter()
 
     useEffect(() => {
-        setTimeout(() => {
-            router.push('/')
-        }, 1000)
+        router.push('/')
     }, [])
-    return (
-        <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-            <div className="max-w-xl mx-auto sm:px-6 lg:px-8">
-                <div className="flex items-center pt-8 sm:justify-start sm:pt-0">
-                    <div className="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
-                        404
-                    </div>
-
-                    <div className="ml-4 text-lg text-gray-500 uppercase tracking-wider">
-                        Страница не найдена
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+    return <div />
 }
+
+// const NotFoundPage = () => {
+//     const router = useRouter()
+
+//     useEffect(() => {
+//         setTimeout(() => {
+//             router.push('/')
+//         }, 1000)
+//     }, [])
+//     return (
+//         <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+//             <div className="max-w-xl mx-auto sm:px-6 lg:px-8">
+//                 <div className="flex items-center pt-8 sm:justify-start sm:pt-0">
+//                     <div className="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+//                         404
+//                     </div>
+
+//                     <div className="ml-4 text-lg text-gray-500 uppercase tracking-wider">
+//                         Страница не найдена
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
 export default NotFoundPage
